029940eb77b6db8b058b6cb090ee4ca1834b7c24
namnthe172571 <namnthe172571@fpt.edu.vn>
2025-12-04 20:21:50 +0700

    logout done

--- src/main/java/controller/common/LogoutServlet.java
+++ src/main/java/controller/common/LogoutServlet.java
@@ -1,29 +1,29 @@
-package controller.user;
+package controller.common;
 
-import dal.UserDAO;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import model.Users;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
-import java.util.List;
 
-@WebServlet(name = "ViewUserListServlet", urlPatterns = {"/admin/user-list"})
-public class ViewUserListServlet extends HttpServlet {
+@WebServlet(name = "LogoutServlet", urlPatterns = {"/logout"})
+public class LogoutServlet extends HttpServlet {
 
-    private static final String USER_LIST_PAGE = "user-manage/view-user-list.jsp";
-    private final UserDAO userDAO = new UserDAO();
+    private static final String LOGIN_PAGE = "login";
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        List<Users> userList = userDAO.getAllUsers();
-        request.setAttribute("userList", userList);
-        request.getRequestDispatcher(USER_LIST_PAGE).forward(request, response);
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+
+        response.sendRedirect(request.getContextPath() + "/" + LOGIN_PAGE);
     }
 
     @Override
